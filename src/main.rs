41fa6c67acaffc090a7d03745e6e9f21876f6857
David Saah <dasorange.hope@gmail.com>
2024-01-23 21:49:03 +0000

    add random number game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+extern crate rand;
+
+use colorz::Colorize;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Type 'exit' to close the program\n");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess: ");
+        let mut guess = String::new();
+
+        // read and process input
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input");
+
+        // convert to integer
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                if guess.trim() == "exit" {
+                    break;
+                } else {
+                    println!("{}", "Invalid input: Please enter a number!!!\n".yellow());
+                    continue;
+                }
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            }
+            Ordering::Greater => println!("{}", "Too big!".red()),
+        }
+
+        println!();
+    }
 }
